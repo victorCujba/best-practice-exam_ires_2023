bc2ae8f73767703c0e8bd00fd05991924cbe64c5
cujbaVictor <cujba.victor96@gmail.com>
2023-08-03 09:37:22 +0200

    Initial commit. Test

--- src/OldFashionPoundApp.java
+++ src/OldFashionPoundApp.java
@@ -1,6 +1,8 @@
+package src;
+
 public class OldFashionPoundApp {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
